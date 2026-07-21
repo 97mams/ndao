509e70a4a1235a7ff0cae3c3aa7576f2ec937131
97mams <97mams@gmail.com>
2026-07-21 20:18:48 +0300

    add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   ssr: {
     noExternal: ["@convex-dev/better-auth"],
   },
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), tsConfigPaths()],
 });
 
 export default config;
