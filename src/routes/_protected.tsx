c891bff9d8f036bec9463f73a34acec7f77627ce
97mams <97mams@gmail.com>
2026-07-21 20:21:09 +0300

    feat: creat new function

---
+++ src/routes/_protected.tsx
@@ -0,0 +1,19 @@
+import { createFileRoute, redirect, Outlet } from '@tanstack/react-router'
+import { getSession } from '@/lib/auth.functions'
+
+export const Route = createFileRoute('/_protected')({
+  beforeLoad: async ({ location }) => {
+    const session = await getSession();
+
+    if (!session) {
+      throw redirect({
+        to: "/Login",
+        search: { redirect: location.pathname },
+      });
+    }
+
+    return { user: session.user };
+  },
+  component: () => <Outlet />,
+}
+)
