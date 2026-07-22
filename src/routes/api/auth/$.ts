41354f0d4e2bd51420f592bbe083ecf400416415
97mams <97mams@gmail.com>
2026-07-22 18:08:07 +0300

    feat: update auth route to use async handler for GET and POST requests

--- src/routes/api/auth/$.ts
+++ src/routes/api/auth/$.ts
@@ -1,11 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { handler } from "~/lib/auth-server";
+import { auth } from '@/lib/auth'
+import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/api/auth/$")({
-  server: {
-    handlers: {
-      GET: ({ request }) => handler(request),
-      POST: ({ request }) => handler(request),
+export const Route = createFileRoute('/api/auth/$')({
+    server: {
+        handlers: {
+            GET: async ({ request }:{ request: Request }) => {
+                return await auth.handler(request)
+            },
+            POST: async ({ request }:{ request: Request }) => {
+                return await auth.handler(request)
+            },
+        },
     },
-  },
-});
+})
