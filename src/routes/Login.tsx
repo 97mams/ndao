fce5250573b2d37e54b8d9a9ca35921f42e25742
97mams <97mams@gmail.com>
2026-07-24 19:58:18 +0300

    feat: update Login route to display loading spinner and include Home component

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,13 +1,19 @@
+import { Home } from '@/components/Home.tsx';
 import { createFileRoute } from '@tanstack/react-router'
+import { Loader } from 'lucide-react';
 
 export const Route = createFileRoute('/Login')({
   loader: () => {
-    return 'Hello World'
+    return <div className="w-full h-[100vh] flex items-center justify-center animate-spin">
+      <Loader />
+    </div>;
   },
   component: LoginComponent,
 })
 
 function LoginComponent() {
   const data = Route.useLoaderData()
-  return <div>{data}</div>
+  return <div>{data}
+  <Home />
+  </div>
 }
