4bccdf4b608791e20c141ceb3f102ebb8e0d9995
97mams <97mams@gmail.com>
2026-07-24 19:58:07 +0300

    feat: simplify IndexComponent by removing session handling and rendering Home component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,11 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { Link, redirect, createFileRoute } from '@tanstack/react-router'
 import { getSession } from '@/lib/auth.functions'
+import { Home } from '@/components/Home.tsx'
 
 export const Route = createFileRoute('/')({
-  beforeLoad: async () => {
-    const session = await getSession();
-
-    if (!session) {
-      throw redirect({ to: "/" });
-    }
-
-    return { user: session.user };
-  },
   component: IndexComponent,
 })
 
 function IndexComponent() {
-  const { user } = Route.useRouteContext();
-  
-  return <div>Welcome, {user.name}!</div>
+  return <div><Home /></div>
 }
