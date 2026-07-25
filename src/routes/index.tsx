520df313fa8e9df7cfc4fb7691875a38ca9f3bc5
97mams <97mams@gmail.com>
2026-07-25 10:54:29 +0300

    feat: add Header component to IndexComponent layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { Link, redirect, createFileRoute } from '@tanstack/react-router'
 import { getSession } from '@/lib/auth.functions'
 import { Home } from '@/components/Home.tsx'
+import { Header } from '@/components/header.tsx';
 
 export const Route = createFileRoute('/')({
   component: IndexComponent,
 })
 
 function IndexComponent() {
-  return <div><Home /></div>
+  return( 
+    <div>
+      <Header />
+      <Home />
+    </div>);
 }
