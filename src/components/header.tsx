f5ed62af636f5321f63475123a94f01bc3021a6e
97mams <97mams@gmail.com>
2026-07-25 10:54:18 +0300

    feat: update Header component to use session prop and remove getSession call

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,22 @@
+
 import { Button } from "@/components/ui/button";
-import { getSession } from "@/lib/auth.functions";
 import { LogOut } from "lucide-react";
 
-export async function Header({ children }: { children: React.ReactNode }) {
-  const session = await getSession()
+export function Header(props:{ session?: any }) {
 
-  console.log(session?.user)
   return (
     <div className="w-full h-15 flex items-center gap-4 justify-end px-4">
       <div className="flex gap-2 justify-around items-center">
-        {children}
-        <Button>
-          <LogOut className="ml-2 h-4 w-4" />
-          Logout
-        </Button>
+        {props.session == undefined ? (
+          <Button variant={"secondary"} size={"sm"}>
+            Se connecter
+          </Button>
+        ) : (
+          <Button variant={"secondary"} size={"sm"}>
+            <LogOut className="mr-2 h-4 w-4" />
+            Déconnexion
+          </Button>
+        )}
       </div>
     </div>
   );
